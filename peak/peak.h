9ab85454445f9c6238251ec3e9dd18e3f00afcf6
hikmat farhat <hfarhat@ndu.edu.lb>
2021-02-23 10:16:10 +0200

    update

--- peak/peak.h
+++ peak/peak.h
@@ -2,7 +2,9 @@
 #include <iterator>
 #include <optional>
 #include <algorithm>
-template <typename Iter>
-	std::optional<Iter> peak(Iter begin, Iter end) {
-			return std::nullopt;
+
+int peak(std::vector<int>& v,int start,int end) {
+		for(int i=1;i<v.size()-1;++i)
+			if(v[i]>v[i-1]&& v[i]>v[i+1])return i;
+		return -1;	
 }
